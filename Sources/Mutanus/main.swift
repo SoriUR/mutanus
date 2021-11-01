6fd11847fc158cd69d1434637ba78cf17fa60b74
Iurii Sorokin <YBSorokin@alfabank.ru>
2021-11-02 01:13:39 +0300

    Added path parsing and validation

--- Sources/Mutanus/main.swift
+++ Sources/Mutanus/main.swift
@@ -1,5 +1,30 @@
 import Foundation
 import ArgumentParser
 
+struct Mutanus: ParsableCommand {
 
-print(FileManager.default.currentDirectoryPath)
+    @Option var path: String?
+
+    func validate() throws {
+        guard let path = path else { return }
+
+        var isDirectory = ObjCBool(true)
+        guard FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory) else {
+            throw ValidationError("Path doesn't exist")
+        }
+
+        guard isDirectory.boolValue else {
+            throw ValidationError("Path is not a folder")
+        }
+    }
+
+    func run() throws {
+        if let path = path {
+            print("custom path: \(path)")
+        } else {
+            print("default path: \(FileManager.default.currentDirectoryPath)")
+        }
+    }
+}
+
+Mutanus.main()
